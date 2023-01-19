332c51fb7a18ce8dccf89855884af7835a1b6ec6
Hanna Mazur <hanna2mazur@gmail.com>
2023-01-19 09:54:15 +0100

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el!)
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input)
+  }
+  return <div>
+    <textarea onChange={e=> setInput(e.target.value)}></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
+    </div>
+    <pre>{code}</pre>
+  </div>
+}
+
+root.render(<App />)
